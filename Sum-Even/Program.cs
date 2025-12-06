d07feb7faeca12d89b1c3e6bc775d62b2306e2ce
Bilkis Nisa <bilkisnisa38@gmail.com>
2025-12-06 10:27:09 +0700

    upload hasil

--- Sum-Even/Program.cs
+++ Sum-Even/Program.cs
@@ -1,2 +1,21 @@
 ﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+//Console.WriteLine("Hello, World!");
+
+
+class Program
+{
+    static void Main()
+    {
+        int sum = 0; 
+        for (int i = 1; i <= 100; i++)
+        {
+            if (i % 2 == 0)
+            {
+                sum += i;
+            }
+        }
+
+        Console.WriteLine("Jumlah semua bilangan genap dari 1 - 100 adalah: " + sum);
+        Console.ReadKey();    
+    }
+}
